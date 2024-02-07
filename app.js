8b004c59b823e01f7ef5f74dc002a104680eb3d4
Ritam2000RK <rtmkayal@gmail.com>
2024-02-08 03:59:20 +0530

    added scroll funtionalities

--- app.js
+++ app.js
@@ -12,12 +12,15 @@ const links = document.querySelector(".links");
 const videoContainer = document.querySelector(".video-container");
 const navHeader = document.querySelector(".nav-header");
 const mainContent = document.getElementById("main");
+const navCenter = document.querySelector('.nav-center');
+const nav = document.getElementById("nav");
+
 
 let navHeaderHeight;
 let linkHeight;
 let containerHeight;
 //totalHeight has problem because if values are null in linkHeight and navHeaderHeight then it can lead to NaN. I'will fix this later.
-let totalHeight = navHeaderHeight + linkHeight; 
+// let totalHeight = navHeaderHeight + linkHeight; 
 
 toggleBtn.addEventListener("click", () => {
   // if(!displayLinks.classList.contains('show-links')) {
@@ -30,13 +33,31 @@ toggleBtn.addEventListener("click", () => {
 
   if (containerHeight === 0) {
     linkHeight = links.getBoundingClientRect().height;
+    let totalHeight = navHeaderHeight + linkHeight;
 
     displayContainer.style.height = `${linkHeight}px`;
     videoContainer.style.marginTop = `${totalHeight}px`;
     mainContent.style.marginTop = `${totalHeight}px`;
   } else {
-    displayContainer.style.height = `0px`;
+    displayContainer.style.height = 0;
     videoContainer.style.marginTop = `${navHeaderHeight}px`;
     mainContent.style.marginTop = `${navHeaderHeight}px`;
   }
 });
+
+window.addEventListener('scroll', () => {
+  const arrowBtn = document.getElementById('arrowBtn');
+  const scrollTop = window.scrollY;
+  const linkHeight = navCenter.getBoundingClientRect().height;
+  if(scrollTop >= linkHeight) {
+    nav.classList.add('fixed-nav');
+  }else{
+    nav.classList.remove('fixed-nav');
+  }
+
+  if(scrollTop > 500) {
+    arrowBtn.classList.add('show-arrowBtn')
+  }else{
+    arrowBtn.classList.remove('show-arrowBtn');
+  }
+})
